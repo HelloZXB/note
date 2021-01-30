675421abb3308df1f490986d9b88d9dfb6073b1b
HelloZXB <Hello16369@163.com>
2021-01-30 17:25:54 +0800

    comment: vssue评论

--- .vuepress/config/pluginsConf.js
+++ .vuepress/config/pluginsConf.js
@@ -10,6 +10,16 @@ module.exports = {
       message: "Found new content available",
       buttonText: "Update"
     }
-  }
+  },
+  '@vssue/vuepress-plugin-vssue': {
+    // 设置 `platform` 而不是 `api`
+    platform: 'github-v4',
+
+    // 其他的 Vssue 配置
+    owner: 'HelloZXB', // github用户名
+    repo: 'note', // 仓库名称
+    clientId: 'be80fbd3a5c32a78eb99', // clientId
+    clientSecret: '6acfc1cc709e476fd116229c2c092f42eb8a38a9', // clientSecret
+  },
 }
 
