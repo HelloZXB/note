810c549ef0cf0f30a94c19ff13f0350968a999bd
HelloZXB <Hello16369@163.com>
2021-02-12 07:24:46 +0800

    feat: Friend Link

--- .vuepress/config/friendLinkConf.js
+++ .vuepress/config/friendLinkConf.js
@@ -10,5 +10,11 @@ module.exports = [
     "desc": "A simple and beautiful vuepress Blog & Doc theme.",
     "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
     "link": "https://vuepress-theme-reco.recoluan.com"
+  },
+  {
+    "title": "Ky的自留地",
+    "desc": "Ky的自留地",
+    "email": "77916819@qq.com",
+    "link": "https://www.kylexk.com/"
   }
 ]
